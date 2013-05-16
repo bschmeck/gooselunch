c97ca0f9a365f47701f058bc32f6af6058d4d0bc
Ben Schmeckpeper <ben@aisle50.com>
2013-05-16 16:49:40 -0500

    Collect parsing code into an object.

--- parse.py
+++ parse.py
@@ -1,37 +1,53 @@
 from datetime import datetime
 import re
 
-# Set up some regexes:
-# Find the URLs of past orders on the past order list page
-order_regex = re.compile("/orders/view_past/(?P<order_id>\d+)")
-# Find the date of an order on a past order page
-date_regex = re.compile("<h3 class.*?>(?P<date_string>.*?) from", re.MULTILINE | re.DOTALL)
-# Find the names of those who ordered on a past order page
-name_regex = re.compile("<td><strong>(?P<name>[A-Z].*?)</strong> wanted")
+from scrape import LunchboxScraper
 
-# Iterate over orders like:
-for m in order_regex.finditer(content):
-    url = m.group(0)
-    order_id = m.group('order_id')
+class ParseError(Exception):
+    pass
 
-    # If we've already processed order_id then continue
+class LunchboxParser:
+    def __init__(self):
+        # Find the URLs of past orders on the past order list page
+        self.order_regex = re.compile("/orders/view_past/(?P<order_id>\d+)")
+        # Find the date of an order on a past order page
+        self.date_regex = re.compile("<h3 class.*?>(?P<date_string>.*?) from", re.MULTILINE | re.DOTALL)
+        # Find the names of those who ordered on a past order page
+        self.name_regex = re.compile("<td><strong>(?P<name>[A-Z].*?)</strong> wanted")
 
-    # For orders we haven't processed, read in the order via url
-    # Store that in order_content
+    def set_orders_page(self):
+        self.orders_page = self.lunchbox.past_orders()
+        
+    def set_order_page(self, order_id):
+        self.order_page = self.lunchbox.past_order(order_id)
+        
+    def parse(self, email, password):
+        self.lunchbox = LunchboxScraper()
+        self.lunchbox.login(email, password)
 
-    # Get the order's date via:
-    date_match = date_regex.search(order_content)
-    if not date_match:
-        raise ParseError("Unable to find date string for order %s" % url)
+        if not self.lunchbox.logged_in:
+            raise ParseError("Invalid email or password.")
+        
+        self.set_orders_page()
+        for order_id in self.order_ids():
+            # TODO: If we've already processed order_id then continue
+            self.set_order_page(order_id)
+            order_date = self.order_date()
+            for name in self.names():
+                # TODO: Check that the person ordered for that day
+                # TODO: Find the person via the name
+                # TODO: Create a LunchOrder object from the order object and the person object
+                pass
+                
+    def order_ids(self):
+        return map(lambda x: x.group('order_id'), self.order_regex.finditer(self.orders_page))
+
+    def order_date(self):
+        date_match = self.date_regex.search(self.order_page)
+        if not date_match:
+            raise ParseError("Unable to find date string for order %s" % url)
     
-    order_date = datetime.strptime(date_match.group('date_string').strip(), "%A, %B %d, %Y")
-
-    # Create order object with order_date and URL
-
-    # Iterate over the names of those who ordered like:
-    for name_match in name_regex.finditer(order_content):
-        name = name_match.group('name')
-
-        # Check that the person ordered for that day
-        # Find the person via the name
-        # Create a LunchOrder object from the order object and the person object
+        return datetime.strptime(date_match.group('date_string').strip(), "%A, %B %d, %Y")
+    
+    def names(self):
+        return map(lambda x: x.group('name'), self.name_regex.finditer(self.order_page))
