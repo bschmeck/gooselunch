92b080dff572b198f16b4fa119cda6440c122830
Ben Schmeckpeper <ben@aisle50.com>
2013-05-16 12:21:26 -0500

    Consolidate scraper code into an object.

--- scrape.py
+++ scrape.py
@@ -7,26 +7,40 @@ import urllib2
 class ParseError(Exception):
     pass
 
-# Logging into lunchbox.fm:
-jar = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
-login_url = "http://lunchbox.fm"
-f = opener.open(login_url)
-login_page = f.read()
-# Grab data[_Token][fields] and data[_Token][key] from the form
-values = {'data[User][email]': 'ben@aisle50.com', 'data[User][password]': 'XXXXX', 'data[User][remember_me]': 0, 'data[_Token][key]': 'KEY', 'data[_Token][fields]': 'FIELDS'}
-data = urllib.urlencode(values)
-req = urllib2.Request(login_url, data, headers)
-f = opener.open(req)
+class LunchboxScraper:
+    def __init__(self):
+        self.jar = cookielib.CookieJar()
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.jar))
+        self.logged_in = False
 
-# Check that we were authenticated
-f.geturl() == 'http://lunchbox.fm/orders'
+    def login(self, email, password):
+        login_url = "http://lunchbox.fm"
+        f = self.opener.open(login_url)
+        login_page = f.read()
 
-# Read in the list of past orders and store in content
-past_orders_url = lunchbox.fm/orders/past
-req = urllib2.Request(past_orders_url, headers=headers)
-f = urllib2.urlopen(req)
-content = f.read()
+        # Grab data[_Token][fields] and data[_Token][key] from the form
+        values = {'data[User][email]': 'ben@aisle50.com', 
+                  'data[User][password]': 'XXXXX', 
+                  'data[User][remember_me]': 0, 
+                  'data[_Token][key]': 'KEY', 
+                  'data[_Token][fields]': 'FIELDS'}
+
+        data = urllib.urlencode(values)
+        req = urllib2.Request(login_url, data)
+        f = self.opener.open(req)
+
+        # Check that we were authenticated
+        self.logged_in = f.geturl() == 'http://lunchbox.fm/orders'
+
+    def past_orders(self):
+        if not self.logged_in:
+            raise Exception("Not logged in.")
+        
+        past_orders_url = lunchbox.fm/orders/past
+        req = urllib2.Request(past_orders_url)
+        f = self.opener.open(req)
+        
+        return f.read()
 
 # Set up some regexes:
 # Find the URLs of past orders on the past order list page
