599366096a3fbe7b3151efdfc01aaeeae675467c
Ben Schmeckpeper <ben@aisle50.com>
2013-05-17 15:48:20 -0500

    Make a database backed model to scrape/parse Lunchbox and populate the database.

--- db_models.py
+++ db_models.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from google.appengine.ext import db
+
+import models
 
 class Person(db.Model):
     """Person who orders lunch."""
@@ -12,7 +16,40 @@ class LunchOrder(db.Model):
 
     person = db.ReferenceProperty(Person, collection_name='lunch_orders')
 
-class ScrapeStats(db.Model):
+class Scraper(db.Model):
     """Data about the current state of scraping."""
-    last_scrape = db.DateProperty()
+    last_scrape = db.DateTimeProperty()
     highest_scraped_id = db.IntegerProperty()
+    lunchbox_email = db.StringProperty()
+    lunchbox_password = db.StringProperty()
+
+    def scrape(self):
+        scraper = models.LunchboxScraper()
+        parser = models.LunchboxParser()
+
+        scraper.login(self.lunchbox_email, self.lunchbox_password)
+        parser.set_scraper(scraper)
+
+        high_seen_id = 0
+        parser.scrape_orders_page()
+        for order_id in parser.order_ids():
+            if order_id <= self.highest_scraped_id:
+                continue
+            if high_seen_id < order_id:
+                high_seen_id = order_id
+            parser.scrape_order_page(order_id)
+                        
+            for name in parser.names():
+                p = Person.all().filter('name =', name).get()
+                if not p:
+                    print "Unknown person", name
+                    continue
+                lunch_order = LunchOrder(date = parser.order_date(),
+                                         restaurant = parser.restaurant(),
+                                         order = '',
+                                         person = p)
+                lunch_order.put()
+        self.last_scrape = datetime.now()
+        self.highest_scraped_id = high_seen_id
+        self.put()
+                
